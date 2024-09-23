3a8c7340208b1fc830a44b5bb19813bcb34ab566
Texas <tanachote.la@gmail.com>
2024-09-24 00:35:48 +0700

    Cells finished!

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const gridBox = document.querySelector('.container');
+// const gridBox = document.querySelector('.container');
 
-let start = 2*2;
-for(let i = 1; i <= start; i++){
-    const row = document.createElement('div');
-    row.className = "row";
-    row.textContent = '';
-    gridBox.appendChild(row);
-    console.log(i)
+// let input = 2;
+// let start = 2;
+// for(let i = 1; i <= start; i++){
+//     const row = document.createElement('div');
+//     row.className = "row";
+//     row.textContent = '';
+//     gridBox.appendChild(row);
+    
+// };
+
+const GRIDSIDE = 600;
+let rows = 16;
+let cols = 16;
+
+const sketchArea = document.querySelector('#sketch-area');
+sketchArea.style.width = `${GRIDSIDE}px`;
+sketchArea.style.height = `${GRIDSIDE}px`;
+
+function createGridCells(){
+    for(let i = 0; i < (rows * cols); i++){
+        const gridCell = document.createElement('div');
+        gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        gridCell.classList.add("cell");
+
+        sketchArea.appendChild(gridCell);
+    };
 };
-// const column = document.createElement('div');
-// column.textContent = '';
-// column.className = "column";
-// gridBox.appendChild(column);
+
+createGridCells();
